d87534be8918e83a32bf0923d302f6e3d1577c46
Jaime Álvarez <96999133+Jaime5Alvarez@users.noreply.github.com>
2023-11-08 19:14:40 +0100

    adds landing page

--- src/components/home/LandingPage.tsx
+++ src/components/home/LandingPage.tsx
@@ -1,7 +1,20 @@
 export const LandingPage = () => {
   return (
     <>
-      <h4>landing page</h4>
+      <section className="translate-y-20">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto">
+          <div className="w-full bg-white rounded-lg drop-shadow-2xl  max-w-md   ">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl text-center font-bold leading-tight tracking-tight  text-blue-800 md:text-2xl ">
+                Welcome to{" "}
+                <span className="bg-blue-900 text-white rounded-xl px-1 py-1">
+                  TO-DO-APP
+                </span>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
